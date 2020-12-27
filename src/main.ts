09eb11471e47808f91a24ce0af901cb43fffb438
joaofavara <joaopedro_favara@hotmail.com>
2020-12-27 13:20:10 -0300

    feat: gerate the api documentation
    - using the swagger with nest and generate the doc for the api

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,22 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 declare const module: any;
 
 async function bootstrap () {
     const app = await NestFactory.create(AppModule);
-    await app.listen(3000);
 
-    if (module.hot) {
-        module.hot.accept();
-        module.hot.dispose(() => app.close());
-    }
+    const options = new DocumentBuilder()
+        .setTitle('Cats example')
+        .setDescription('The cats API description')
+        .setVersion('1.0')
+        .build();
+    const document = SwaggerModule.createDocument(app, options);
+    SwaggerModule.setup('api', app, document);
+
+    await app.listen(3000);
+    console.log(`Application is running on: ${await app.getUrl()}`);
 };
 
 bootstrap();
